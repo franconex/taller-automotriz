5e9d769354a2decae94a07caed26ce6d4709b08d
franconex <ismaelfrancoruiz3007@gmail.com>
2026-07-26 23:51:24 -0400

    Quitar TestDataSeeder del DatabaseSeeder (no ejecutar automaticamente)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
             RolPermisoSeeder::class,
             MetodoPagoSeeder::class,
             UserSeeder::class,
-            TestDataSeeder::class,
         ]);
     }
 }
